6491fdbd1894d062ddbd98a68fb49d953f648a9b
F3de22 <monettifederico22@gmail.com>
2024-06-19 20:52:32 +0200

    u

--- PPM_elaborato_backend/settings.py
+++ PPM_elaborato_backend/settings.py
@@ -141,7 +141,7 @@ TEMPLATE_DIRS = (
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
